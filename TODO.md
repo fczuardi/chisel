fa15a99ada17d41bd3dcb96b5af255c2811d9830
Fabricio C Zuardi <fabricio@fabricio.org>
2011-04-11 19:29:31 -0300

    new version of the colapsable theme: - snippets! - collapsing a big item auto scrolls back to that item instead of dragging the next items out of the visible area

--- TODO.md
+++ TODO.md
@@ -4,8 +4,9 @@
 # colapsable-forrst
 From the [original Forrst post](http://forrst.com/posts/Colapsable_Forrst_Timeline_experimental_mod-L55)
 
+* OK <s>idea: be able to se a short snippet of the text on collapsed items.</s>
+* OK <s>improvement: when you colapse an expanded item that have the top out of the visible area the page should scroll up in order to not mis items</s>
+* WONTFIX <s>idea: use CSS transitions to animate the expand / collapse</s>
 * bug: when you click on the heart to like an expanded item, it is currently collapsing the item sometimes, It shouldn't.
-* idea: be able to se a short snippet of the text on collapsed items.
 * idea: prevent the loading of the images before expanding the item, in order to load faster and save bandwidth for phone users.
-* idea: use CSS transitions to animate the expand / collapse
 * idea: show a "next item" link, similar to GReader when the item is expanded
--- themes/colapsable/user.js
+++ themes/colapsable/user.js
@@ -7,13 +7,22 @@ function tweakUpdates(){
         classname = $(this).children('.activity-inner').children('.activity-meta').children().last().html().substring(2);
       } catch(e){}
       $(this).addClass(classname);
+      $(this).addClass('closed');
       $(this).click(function(e) {
-        $(this).children('.activity-inner').toggleClass('open');
+        $(this).toggleClass('open closed');
+        if ($(this).hasClass('closed')){
+          $(document).scrollTop($(this).offset().top);
+        }
       });
     }
   });
 }
 function init(){
+  // Forrst.applyLibs is the function that the page call when the DOM is ready
+  // and after every new pagination is loaded, so we extend it and use as the
+  // hook to run the custom modifications (tweakUpdates).
+  // If the Forrst object is not present, it means that the Forrst javascript 
+  //is not loaded yet, so we wait for 2 secconds before trying again.
   if (typeof Forrst != 'undefined') {
     applyLibsCopy = Forrst.applyLibs;
     extendedApplyLibs = function(){
