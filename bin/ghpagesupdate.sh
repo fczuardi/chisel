ce1f1bd7fd81f83c7ed811747b4b5cc508d3026b
Fabricio C Zuardi <fabricio@fabricio.org>
2011-04-14 00:12:40 -0300

    trying to not upload all submodules

--- bin/ghpagesupdate.sh
+++ bin/ghpagesupdate.sh
@@ -4,13 +4,22 @@ if [ "$foo" = "" ]
 then 
   echo "Please commit your changes first."
 else
+  echo "rm -rf /tmp/chisel-builds"
   rm -rf /tmp/chisel-builds
+  echo "cp -r ./builds /tmp/chisel-builds"
   cp -r ./builds /tmp/chisel-builds
+  echo "git checkout gh-pages"
   git checkout gh-pages
+  echo "rm -rf builds"
   rm -rf builds
+  echo "mv /tmp/chisel-builds ./builds"
   mv /tmp/chisel-builds ./builds
-  git add .
+  echo "git add builds"
+  git add builds
+  echo "git commit -am \"auto updated builds folder\""
   git commit -am "auto updated builds folder"
+  echo "git push origin gh-pages"
   git push origin gh-pages
+  echo "git checkout master"
   git checkout master
 fi
