005c5447cbcb8a74d8d757507de34310301c3fac
Fabricio C Zuardi <fabricio@fabricio.org>
2011-04-13 22:47:18 -0300

    progress on the shel script

--- bin/ghpagesupdate.sh
+++ bin/ghpagesupdate.sh
@@ -1,1 +1,15 @@
-cp -r builds /tmp/chisel-builds
+#!/bin/bash
+foo=`git status|grep "nothing to commit"`
+if [ "$foo" = "" ]
+then 
+  echo "Please commit your changes first."
+else
+  echo "yay!"
+  cp -r ./builds /tmp/chisel-builds
+  git checkout gh-pages
+  cp -r /tmp/chisel-builds ./builds
+  git add .
+  git commit -m "auto updated builds folder"
+  git push origin gh-pages
+  git checkout master
+fi
